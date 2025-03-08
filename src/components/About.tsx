ce1403c78c0719f368d758a51011bb8a42c0d2d5
WuTangLan1 <finnmassari16@gmail.com>
2025-03-08 13:55:01 +0200

    enhanced about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,10 +7,16 @@ const About: React.FC = () => {
   const { darkMode } = useThemeContext();
   return (
     <section id="about" className={`about-section ${darkMode ? 'dark-mode' : 'light-mode'}`}>
-      <img src="/images/profile_photos/profile1.png" alt="Profile" className="about-image" />
-      <h1>About Citizen Kane</h1>
-      <p>Citizen Kane is a rising comedy sensation known for sharp wit, engaging storytelling, and unforgettable performances.</p>
-      <p>From small local venues to major comedy clubs across the globe, Citizen Kane continues to captivate audiences with hilariously relatable tales.</p>
+      <div className="about-container">
+        <img src="/images/profile_photos/profile1.png" alt="Profile" className="about-image" />
+        <div className="about-content">
+          <h1>About Citizen Kane</h1>
+          <p>Citizen Kane is a rising comedy sensation known for sharp wit, engaging storytelling, and unforgettable performances.</p>
+          <p>From small local venues to major comedy clubs across the globe, Citizen Kane continues to captivate audiences with hilariously relatable tales.</p>
+          <p>His unique blend of humor and insight pushes the boundaries of comedy while forging a deep connection with his audience.</p>
+          <p>His journey in comedy is a testament to perseverance, creativity, and the power of laughter to unite people from all walks of life.</p>
+        </div>
+      </div>
     </section>
   );
 };
